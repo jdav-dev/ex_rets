c02a4359e26b1035463c5fb509fa465eb6f0f6b9
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2019-10-04 09:48:17 -0400

    Output search results as columns and rows

--- lib/ex_rets/search_response.ex
+++ lib/ex_rets/search_response.ex
@@ -3,24 +3,19 @@ defmodule ExRets.SearchResponse do
 
   @type t :: %__MODULE__{
           count: non_neg_integer(),
-          results: [%{optional(String.t()) => String.t()}]
+          columns: [String.t()],
+          rows: [String.t()]
         }
 
-  defstruct count: nil, results: []
+  defstruct count: nil, columns: [], rows: []
 
   def from_rets_response(%RetsResponse{response: response}) do
     count = get_count(response)
     delimiter = get_delimiter(response)
     columns = get_columns(response, delimiter)
-    datas = get_datas(response, delimiter)
+    rows = get_datas(response, delimiter)
 
-    results =
-      datas
-      |> pair_datas_with_columns(columns)
-      |> filter_empty_data()
-      |> convert_datas_to_maps()
-
-    %__MODULE__{count: count, results: results}
+    %__MODULE__{count: count, columns: columns, rows: rows}
   end
 
   defp get_count(response) do
@@ -61,19 +56,5 @@ defmodule ExRets.SearchResponse do
     |> Stream.map(&Map.get(&1, :elements))
     |> Stream.map(&List.first/1)
     |> Enum.map(&String.split(&1, delimiter))
-  end
-
-  defp pair_datas_with_columns(datas, columns) do
-    Stream.map(datas, &Enum.zip(columns, &1))
-  end
-
-  defp filter_empty_data(stream) do
-    stream
-    |> Stream.map(&Enum.reject(&1, fn {_, v} -> is_nil(v) or v == "" end))
-    |> Stream.reject(&(&1 == %{}))
-  end
-
-  defp convert_datas_to_maps(stream) do
-    Enum.map(stream, &Enum.into(&1, %{}))
   end
 end
