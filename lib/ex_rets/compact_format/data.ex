52a31d30312342a4374c8e373158df5f044ff868
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2019-10-09 09:57:30 -0400

    Fixing how trailing delimiters are cut

--- lib/ex_rets/compact_format/data.ex
+++ lib/ex_rets/compact_format/data.ex
@@ -9,19 +9,17 @@ defmodule ExRets.CompactFormat.Data do
   @spec parse(data :: String.t(), options()) :: t()
   def parse(data, opts \\ []) when is_binary(data) and is_list(opts) do
     delimiter = opts[:delimiter] || "\t"
-
+    split_data = String.split(data, delimiter)
+    length_split_data = length(split_data)
     start_index = if String.starts_with?(data, delimiter), do: 1, else: 0
-    length_data = String.length(data)
 
     amount =
       if String.ends_with?(data, delimiter) do
-        length_data - start_index - 1
+        length_split_data - start_index - 1
       else
-        length_data - start_index
+        length_split_data - start_index
       end
 
-    data
-    |> String.split(delimiter)
-    |> Enum.slice(start_index, amount)
+    Enum.slice(split_data, start_index, amount)
   end
 end
