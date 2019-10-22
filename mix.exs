d4c2bd19a7f30e094dbd2a8ea49c5b56719463ff
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2019-10-22 12:12:00 -0400

    Adding xmerl to the list of extra applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ExRets.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :inets, :ssl]
+      extra_applications: [:logger, :inets, :ssl, :xmerl]
     ]
   end
 
