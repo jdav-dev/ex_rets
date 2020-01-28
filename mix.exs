f020ac1a3fda6ce3d13e8d0213365fd64507b15a
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2020-01-28 09:31:14 -0500

    Updating project metadata

--- mix.exs
+++ mix.exs
@@ -7,33 +7,13 @@ defmodule ExRets.MixProject do
     [
       app: :ex_rets,
       version: @version,
-      elixir: "~> 1.9",
-      package: package(),
-      description: "RETS client for Elixir",
+      elixir: "~> 1.10",
+      name: "ExRets",
+      description: "RETS client for Elixir.",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [
-        main: "ExRets",
-        source_ref: "v#{@version}",
-        source_url: "https://github.com/jdavis92/ex_rets",
-        groups_for_modules: [
-          Login: [
-            ExRets.CapabilityUris,
-            ExRets.Credentials,
-            ExRets.LoginResponse,
-            ExRets.SessionInformation
-          ],
-          Logout: [
-            ExRets.LogoutResponse
-          ],
-          Search: [
-            ExRets.CompactDelimiter,
-            ExRets.CompactRecord,
-            ExRets.SearchArguments,
-            ExRets.SearchResponse
-          ]
-        ]
-      ]
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -51,10 +31,34 @@ defmodule ExRets.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "ExRets",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/jdavis92/ex_rets",
+      groups_for_modules: [
+        Login: [
+          ExRets.CapabilityUris,
+          ExRets.Credentials,
+          ExRets.LoginResponse,
+          ExRets.SessionInformation
+        ],
+        Logout: [
+          ExRets.LogoutResponse
+        ],
+        Search: [
+          ExRets.CompactDelimiter,
+          ExRets.CompactRecord,
+          ExRets.SearchArguments,
+          ExRets.SearchResponse
+        ]
+      ]
+    ]
+  end
+
   defp package do
     %{
       licenses: ["Apache-2.0"],
-      maintainers: ["Josh Davis"],
       links: %{"GitHub" => "https://github.com/jdavis92/ex_rets"}
     }
   end
