22b47fdf44440e54ee19ed1414097086a77524d9
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2020-01-28 09:31:14 -0500

    Updating project metadata

--- mix.exs
+++ mix.exs
@@ -7,33 +7,13 @@ defmodule ExRets.MixProject do
     [
       app: :ex_rets,
       version: @version,
-      elixir: "~> 1.9",
-      package: package(),
-      description: "RETS client for Elixir",
+      elixir: "~> 1.10",
+      name: "ExRets",
+      description: "RETS client for Elixir.",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [
-        main: "ExRets",
-        source_ref: "v#{@version}",
-        source_url: "https://github.com/jdav-dev/ex_rets",
-        groups_for_modules: [
-          Login: [
-            ExRets.CapabilityUris,
-            ExRets.Credentials,
-            ExRets.LoginResponse,
-            ExRets.SessionInformation
-          ],
-          Logout: [
-            ExRets.LogoutResponse
-          ],
-          Search: [
-            ExRets.CompactDelimiter,
-            ExRets.CompactRecord,
-            ExRets.SearchArguments,
-            ExRets.SearchResponse
-          ]
-        ]
-      ]
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -48,6 +28,31 @@ defmodule ExRets.MixProject do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
       {:credo, "~> 1.1", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "ExRets",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/jdav-dev/ex_rets",
+      groups_for_modules: [
+        Login: [
+          ExRets.CapabilityUris,
+          ExRets.Credentials,
+          ExRets.LoginResponse,
+          ExRets.SessionInformation
+        ],
+        Logout: [
+          ExRets.LogoutResponse
+        ],
+        Search: [
+          ExRets.CompactDelimiter,
+          ExRets.CompactRecord,
+          ExRets.SearchArguments,
+          ExRets.SearchResponse
+        ]
+      ]
     ]
   end
 
